4de4d18c47de3de577c81d868f88939f5bb01bce
LakshanKavindu <lakshankavindu2k21@gmail.com>
2023-02-23 10:38:15 +0530

    create db.sql

---
+++ db.sql
@@ -0,0 +1,14 @@
+create database todo_db;
+use todo_db;
+CREATE TABLE `new_table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `todoname` varchar(45) DEFAULT NULL,
+  `is_deleted` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+insert into new_table ('username','todoname','is_deleted') values("nipun","do react",0);
+insert into new_table ('username','todoname','is_deleted') values("gattu","do react and vanilla js",0);
+insert into new_table ('username','todoname','is_deleted') values("charuka","do shiny stuff",0);
